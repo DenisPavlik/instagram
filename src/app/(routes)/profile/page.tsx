b7f31611b75de2888ac99fe1e03c69f6bbee3dd5
Denys Pavlyk <denispavlik9@gmail.com>
2026-01-22 21:56:30 -0500

    Edited settings

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -7,14 +7,14 @@ export default function Profile() {
     <div>
       <div className="flex items-center justify-between">
         <button className="group relative px-3 py-2 overflow-hidden">
-          <div className="flex items-center">
+          <Link href={'/'} className="flex items-center">
             <ChevronLeft className="transition-all duration-500 group-hover:opacity-0 group-hover:-translate-x-2" />
             <ChevronLeft className="absolute left-3 opacity-0 translate-x-2 transition-all duration-500 group-hover:opacity-100 group-hover:translate-x-0" />
-          </div>
+          </Link>
         </button>
 
         <div className="font-bold flex items-center gap-2">
-          my name is CJ
+          john_doe
           <div className="size-5 rounded-full bg-cRed inline-flex justify-center items-center text-white">
             <CheckIcon size={16} />
           </div>
@@ -39,8 +39,8 @@ export default function Profile() {
       </section>
       <section className="text-center mt-4">
         <h1 className="text-bold text-xl">John</h1>
-        <p className="text-gray-500 my-1">Business account</p>
-        <p>Entrepreneur, CEO of RoboTech</p>
+        <p className="text-gray-400 my-1">Business account</p>
+        <p className="text-sm">Entrepreneur, CEO of RoboTech</p>
       </section>
       <section className="mt-4">
         <div className="flex justify-center gap-4 font-semibold">
