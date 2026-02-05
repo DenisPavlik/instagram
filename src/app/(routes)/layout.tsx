d678de593c4439253ae2b6cac0aa6c1db452485a
Denys Pavlyk <denispavlik9@gmail.com>
2026-02-05 15:00:34 -0500

    Added mobile navigation

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -4,7 +4,14 @@ import "../globals.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import Link from "next/link";
-import { CameraIcon, GridIcon, HomeIcon, LayoutGrid, SearchIcon, User2Icon } from "lucide-react";
+import {
+  CameraIcon,
+  GridIcon,
+  HomeIcon,
+  LayoutGrid,
+  SearchIcon,
+  User2Icon,
+} from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,14 +29,29 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} p-4`}>
-        <Theme>{children}
-          <div className="fixed bottom-0 bg-white px-8 py-6 left-0 right-0">
-            <div className="flex items-center gap-6 justify-between">
-              <Link href="/"><HomeIcon /></Link>
-            <Link href="/search"><SearchIcon /></Link>
-            <Link href="/create"><CameraIcon /></Link>
-            <Link href="/browse"><LayoutGrid /></Link>
-            <Link href="/profile"><User2Icon /></Link>
+        <Theme>
+          {children}
+          <div className="fixed bottom-0 bg-white px-4 py-2 left-0 right-0">
+            <div className="max-w-sm mx-auto flex items-center gap-6 justify-between *:size-12 *:flex *:items-center *:justify-center">
+              <Link href="/">
+                <HomeIcon />
+              </Link>
+              <Link href="/search">
+                <SearchIcon />
+              </Link>
+              <Link
+                href="/create"
+                className="bg-gradient-to-tr from-cOrange to-cRed text-white size-12 flex
+                items-center justify-center rounded-full relative -top-6 shadow-md shadow-gray-400"
+              >
+                <CameraIcon />
+              </Link>
+              <Link href="/browse">
+                <LayoutGrid />
+              </Link>
+              <Link href="/profile">
+                <User2Icon />
+              </Link>
             </div>
           </div>
         </Theme>
